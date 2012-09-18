809ded1f5e319eb076807e5812ff7565f3d9cbcd
Andy Arias <andyarias17@gmail.com>
2012-09-18 13:09:15 -0500

    Hola, primer commit andy

---
+++ APrueba/src/main/java/GPrueba0/APrueba/andy.java
@@ -0,0 +1,13 @@
+package GPrueba0.APrueba;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
