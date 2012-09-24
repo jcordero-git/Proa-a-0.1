7b381a2a3f634efe5c49a2994ad470d150c94b01
Luis Zuniga <luiszunigacr@gmail.com>
2012-09-23 22:06:31 -0600

    Primer commit - Luis

--- APrueba/src/main/java/GPrueba0/APrueba/andy.java
+++ APrueba/src/main/java/GPrueba0/APrueba/andy.java
@@ -3,11 +3,12 @@ package GPrueba0.APrueba;
 /**
  * Hello world!
  *
+ * Updated 09-23-2012 
  */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!!" );
     }
 }
